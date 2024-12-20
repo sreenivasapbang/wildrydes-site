4e4388c3def7dd17a1cb57726b6375cfa2deb0df
sreenivasapbang <66455934+sreenivasapbang@users.noreply.github.com>
2024-12-20 13:19:50 +0530

    Update cognito-auth.js

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -8,7 +8,7 @@ var WildRydes = window.WildRydes || {};
     var poolData = {
         UserPoolId: _config.cognito.userPoolId,
         ClientId: _config.cognito.userPoolClientId,
-        ClientSecret: _config.cognito.userClientSecret
+        ClientIdSecret: _config.cognito.userClientSecret
     };
 
     var userPool;
