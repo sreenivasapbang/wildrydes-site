cf4d246025503a8ba04b93b64933a1a61c55d53f
sreenivasapbang <66455934+sreenivasapbang@users.noreply.github.com>
2024-12-20 13:24:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_dIClLeqh6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5949figm9bf9imb28qktldjugf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5ud3gg2e74ess5m34u5ouralm3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         userClientSecret: '2b59j9lafhuiqk9uv1sv8ma69q6voppafthtpu4dklois655cti',
         region: 'ap-south-1' // e.g. us-east-2
     },
